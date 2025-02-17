b8b3a6500fe868ee39075c7cc4e7a97bc82effab
aqeebpathan <developer.aqeeb@gmail.com>
2025-02-17 21:08:02 +0530

    fix: add delay before showing pop-up

--- frontend/src/components/CreditPopup.tsx
+++ frontend/src/components/CreditPopup.tsx
@@ -1,15 +1,25 @@
-import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
 const CreditPopup = () => {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
   const popupRef = useRef<HTMLDivElement>(null);
 
-  // Close popup when clicking outside
+  useEffect(() => {
+    const isClosed = localStorage.getItem("popupClosed");
+    if (!isClosed) {
+      const timer = setTimeout(() => {
+        setIsVisible(true);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
       if (popupRef.current && !popupRef.current.contains(e.target as Node)) {
         setIsVisible(false);
+        localStorage.setItem("popupClosed", "true");
       }
     };
 
@@ -18,6 +28,11 @@ const CreditPopup = () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
+
+  const closePopup = () => {
+    setIsVisible(false);
+    localStorage.setItem("popupClosed", "true");
+  };
 
   return (
     <>
@@ -29,12 +44,12 @@ const CreditPopup = () => {
           >
             <h2 className="text-xl font-semibold mb-4">Music Credits</h2>
 
-            <p className="mb-4 text-neutral-400 text-sm ">
+            <p className="mb-4 text-neutral-400 text-sm">
               We are deeply grateful to the talented artists whose music
               enhances this experience.
             </p>
 
-            <p className="mb-4 text-neutral-400 text-sm ">
+            <p className="mb-4 text-neutral-400 text-sm">
               All music featured on this platform remains the property of its
               respective creators. This project is non-commercial and built for
               learning purposes, with full respect for the original artists.
@@ -44,7 +59,7 @@ const CreditPopup = () => {
               <Link
                 to="/credits"
                 className="text-neutral-50 group font-medium text-sm underline underline-offset-2"
-                onClick={() => setIsVisible(false)}
+                onClick={closePopup}
               >
                 View Full Music Credits
               </Link>
@@ -55,7 +70,7 @@ const CreditPopup = () => {
             </p>
 
             <button
-              onClick={() => setIsVisible(false)}
+              onClick={closePopup}
               className="mt-5 bg-neutral-100 text-neutral-950 text-sm font-semibold py-2 px-4 rounded hover:opacity-85 cursor-pointer tracking-tight w-full"
             >
               Close
