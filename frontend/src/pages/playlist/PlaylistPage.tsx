20ab78eef81a3bd39f100a0ec8a0919dda3b382f
aqeebpathan <developer.aqeeb@gmail.com>
2025-02-21 22:58:02 +0530

    fix: improve UX by adding playlist song existence checks

--- frontend/src/pages/playlist/PlaylistPage.tsx
+++ frontend/src/pages/playlist/PlaylistPage.tsx
@@ -96,82 +96,94 @@ const PlaylistPage = () => {
             <ActionMenu />
           </div>
 
-          {/* table section*/}
-          <div className="">
-            <div className="grid grid-cols-[16px_4fr_1fr] gap-4 px-6  py-2 text-sm text-neutral-400 border-b border-white/5">
-              <div className="flex justify-center items-center ">
-                <ListMinus size={16} />
-              </div>
-              <div>Title</div>
-              <div>
-                <Clock className="h-4 w-4" />
-              </div>
+          {currentPlaylist?.songs.length === 0 ? (
+            <div className="text-neutral-400 px-6">
+              Playlist is empty, add some songs!
             </div>
-          </div>
-
-          {/* songs list */}
-          <div className="px-1">
-            <div className="space-y-2 py-4">
-              {currentPlaylist?.songs.map((song, index) => {
-                const isCurrentSong = currentSong?._id === song._id;
-                return (
-                  <div
-                    key={song._id}
-                    onClick={() => handlePlaySong(index)}
-                    className={`grid grid-cols-[16px_4fr_1fr] gap-4 px-4 py-2 text-sm text-neutral-400 hover:bg-neutral-800/90 rounded-md group cursor-pointer`}
-                  >
-                    <div className="flex items-center justify-center">
-                      {!isCurrentSong && (
-                        <span className="group-hover:hidden">{index + 1}</span>
-                      )}
-
-                      {isCurrentSong && !isPlaying && (
-                        <Triangle
-                          fill="#e5e5e5"
-                          className="size-4  text-neutral-200 rotate-90"
-                        />
-                      )}
-
-                      {isPlaying && isCurrentSong && <SongPlayingAnimation />}
-
-                      {!currentSong && (
-                        <Triangle
-                          fill="#e5e5e5"
-                          className="size-4 hidden group-hover:block text-neutral-200 rotate-90"
-                        />
-                      )}
-
-                      {currentSong && currentSong._id !== song._id && (
-                        <Triangle
-                          fill="#e5e5e5"
-                          className="size-4 hidden group-hover:block text-neutral-200 rotate-90"
-                        />
-                      )}
-                    </div>
-
-                    <div className="flex items-center gap-3">
-                      <img
-                        src={song.imageURL}
-                        alt={song.title}
-                        className="size-10 rounded"
-                      />
-
-                      <div>
-                        <div className={`font-medium text-white`}>
-                          {song.title}
-                        </div>
-                        <div>{song.artist}</div>
-                      </div>
-                    </div>
-
-                    <div className="flex items-center">
-                      {formatDuration(song.duration)}
-                    </div>
+          ) : (
+            <>
+              {/* table section*/}
+              <div className="">
+                <div className="grid grid-cols-[16px_4fr_1fr] gap-4 px-6  py-2 text-sm text-neutral-400 border-b border-white/5">
+                  <div className="flex justify-center items-center ">
+                    <ListMinus size={16} />
                   </div>
-                );
-              })}
-            </div>
-          </div>
+                  <div>Title</div>
+                  <div>
+                    <Clock className="h-4 w-4" />
+                  </div>
+                </div>
+              </div>
+              {/* songs list */}
+              <div className="px-1">
+                <div className="space-y-2 py-4">
+                  {currentPlaylist?.songs.map((song, index) => {
+                    const isCurrentSong = currentSong?._id === song._id;
+                    return (
+                      <div
+                        key={song._id}
+                        onClick={() => handlePlaySong(index)}
+                        className={`grid grid-cols-[16px_4fr_1fr] gap-4 px-4 py-2 text-sm text-neutral-400 hover:bg-neutral-800/90 rounded-md group cursor-pointer`}
+                      >
+                        <div className="flex items-center justify-center">
+                          {!isCurrentSong && (
+                            <span className="group-hover:hidden">
+                              {index + 1}
+                            </span>
+                          )}
+
+                          {isCurrentSong && !isPlaying && (
+                            <Triangle
+                              fill="#e5e5e5"
+                              className="size-4  text-neutral-200 rotate-90"
+                            />
+                          )}
+
+                          {isPlaying && isCurrentSong && (
+                            <SongPlayingAnimation />
+                          )}
+
+                          {!currentSong && (
+                            <Triangle
+                              fill="#e5e5e5"
+                              className="size-4 hidden group-hover:block text-neutral-200 rotate-90"
+                            />
+                          )}
+
+                          {currentSong && currentSong._id !== song._id && (
+                            <Triangle
+                              fill="#e5e5e5"
+                              className="size-4 hidden group-hover:block text-neutral-200 rotate-90"
+                            />
+                          )}
+                        </div>
+
+                        <div className="flex items-center gap-3">
+                          <img
+                            src={song.imageURL}
+                            alt={song.title}
+                            className="size-10 rounded"
+                          />
+
+                          <div>
+                            <div className={`font-medium text-white`}>
+                              {song.title}
+                            </div>
+                            <div>{song.artist}</div>
+                          </div>
+                        </div>
+
+                        <div className="flex items-center">
+                          {formatDuration(song.duration)}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </>
+          )}
+          <></>
         </div>
       </ScrollArea>
     </div>
